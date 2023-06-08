c4a6253baaa60dafe3955d1905f8a39a16b156f2
MuhammadSubhan98 <subhan@deltalabs.io>
2023-06-08 20:45:26 +0500

    Final version of one to many version

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,9 @@ class GamesController < ApplicationController
     def show
     end
 
+    def towerofhonoi
+    end
+
     def tictactoe
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root 'users#show', id: 1
   resources :games, only: [:show]
-  resources :users, except: [:new]
+  resources :users, only: [:show]
+  get 'towerofhonoi', to: 'games#towerofhonoi'
   get 'tictactoe', to: 'games#tictactoe'
   get 'rockpaperscissor', to: 'games#rockpaper'
 end
