55f18c205029f42ff377d4f6784a04d1a321051c
Sabir Mujahid <sabiirmujahid@gmail.com>
2023-07-31 21:41:33 +0500

    added devise base authentication and removed session base authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,3 @@
 class ApplicationController < ActionController::Base
 
-    #this is application controller
-    helper_method :current_user, :logged_in?
-    
-    def current_user
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
-
-    def logged_in?
-        !!current_user
-    end
-
-    def require_user
-        if !logged_in?
-            flash[:alert]="You must be logged in first"
-            redirect_to login_path
-        end
-    end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,56 +1,13 @@
 class UsersController < ApplicationController
-    before_action :set_user, only:[:update, :edit, :show]
-    before_action :require_user, only: [:update, :edit]
-    before_action :require_same_user, only: [:update, :edit, :show]
+    before_action :set_user, only:[:show]
 
     def show
     end
 
-    def new
-        @user = User.new
-    end
-
-    def edit
-    end
-
-    def update
-        if @user.update(user_paramss)
-            flash[:notice]="You have successfully updated your games"
-            redirect_to @user
-        else
-            render 'edit'
-        end
-    end
-
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:notice]= "Welcome to the Online Games, You've successfully signed up"
-            redirect_to login_path
-        else
-            render 'new'
-        end
-    end
-
     private
-
-    def user_params
-        params.require(:user).permit(:username, :email, :password, game_ids: [])
-    end
-
-    def user_paramss
-        params.require(:user).permit(:username, game_ids: [])
-    end
 
     def set_user
         @user= User.find(params[:id])
-    end
-
-    def require_same_user
-        if current_user != @user
-            flash[:alert]="You cannot access this account"
-            redirect_to current_user
-        end
     end
 
 end
